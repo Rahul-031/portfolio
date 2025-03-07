35c87d8deacb6fa5262970dfd943584b2b9ae8b1
Rahul-031 <rahuldangre321@gmail.com>
2025-03-07 16:34:23 +0530

    initial portfolio commit

---
+++ data/index.ts
@@ -0,0 +1,234 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  // {
+  //   id: 5,
+  //   title: "Currently building a JS Animation library",
+  //   description: "The Inside Scoop",
+  //   className: "md:col-span-3 md:row-span-2",
+  //   imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+  //   titleClassName: "justify-center md:justify-start lg:justify-center",
+  //   img: "/b5.svg",
+  //   spareImg: "/grid.svg",
+  // },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+ 
+  {
+    id: 1,
+    title: "Freelance Web development",
+    desc: `Voyaah.com is a leisure travel website that plans end-to-end custom vacations and curates the best-valued luxury stays
+            ∗ Developed microfrontends on next.js for various business solutions. For example - managing leads, creating opportunity, creating booking, campagin management etc.
+            ∗ fixing ui and back-end bugs across the platform.
+            ∗ Played a key role in the development of a lead management platform, involving tasks such as extracting data from xlsx files, integrating WhatsApp components, ultimately improving the efficiency and functionality of  the system.`,
+    className: "md:col-span-2", // change to md:col-span-2
+    // thumbnail: "https://web-stg.raven.voyaah.com/assets/brand-logo-H7zAdFYK.svg",
+    thumbnail: "/v.png",
+
+  },
+  {
+    id: 2,
+    title: "Freelance Web development",
+    desc: `Onpsend is an application that Simplifies cloud billing and budget management for Google Cloud Platform(GCP) and Google Maps Platform.
+            ∗ Developed a Cloud reseller billing application to fix existing bugs and automate the reconciliation process.
+            ∗ Utilized various GCP services, including GAE, BigQuery, DataStore, Cloud Storage, Secret Manager, Cloud Functions and 
+                Cloud scheduler.
+            ∗ Made effective updates to frontend code (Angular) and backend code (Java) to resolve user issues.
+            ∗ Worked around BigQuery, SQL and app engine to optimize the report generation process and reduced the manual cleanup done on the generated reports. `,
+    className: "md:col-span-2", // change to md:col-span-2
+    // thumbnail: "https://help.onspend.onixnet.com/img/onspend-full-white.png",
+    thumbnail: "/oso.png",
+  },
+  {
+    id: 3,
+    title: "Software Development Engineer",
+    desc: "At Brillio, I have been actively involved in designing and developing innovative web and mobile solutions, leveraging modern technologies. One of my key contributions is **Brillio On The Go**, where I meticulously crafted a highly adaptable and reusable **custom calendar component** using **React.js and React-Native**. This calendar component facilitates smooth navigation between months and seamless interaction with date-specific data. I implemented **efficient state management** to handle real-time rendering and updates, ensuring an interactive and intuitive user experience. Through this project, I demonstrated expertise in **React’s component-based architecture, JSX, Tailwind CSS, and responsive design principles**. ",
+    className: "md:col-span-2", // change to md:col-span-2
+    // thumbnail: "https://www.brillio.com/wp-content/themes/brillio/assets/images/logo/Brillio_Secondary-Logo.svg",
+    thumbnail: "/b.png",
+  },
+  {
+    id: 4,
+    title: "Frontend Engineer Intern",
+    desc: "AppXBuild Technologies Private Limtied is an innovative tech company where I worked as a Node.js Developer, contributing to the development of APIs and gaining expertise in React.js and JavaScript. During my tenure, I was involved in creating a revolutionary chat application and a live weather app, both showcasing real-time capabilities and user-friendly interfaces. The company's client-centric approach fostered a culture of continuous learning, enabling me to stay updated with the latest technologies while delivering tailored solutions that exceeded clients expectations.",
+    className: "md:col-span-2",
+    // thumbnail: "https://www.appxbuild.com/wp-content/uploads/2023/04/AppxBuild-2000x1100-1.png",
+    thumbnail: "/apx.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    description:"gitHub"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    description:"twitter"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    description:"linkedIn"
+  },
+];
